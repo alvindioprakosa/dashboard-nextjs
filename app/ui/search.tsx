825de46890e766c8925a90da3615734e043a1163
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-14 13:17:05 +0700

    Update Search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,26 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { ChangeEvent } from "react";
 
-export default function Search({ placeholder }: { placeholder: string }) {
+type SearchProps = {
+  placeholder?: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Search({ placeholder = "Search...", value, onChange }: SearchProps) {
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        id="search"
+        name="search"
+        type="search"
+        value={value}
+        onChange={onChange}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
