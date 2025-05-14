27898ee347c01cbb3facec0a17e4c714985866cc
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-14 13:18:43 +0700

    Update Dashboard Navlinks

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,14 +4,15 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 
-// Links configuration to display in the side navigation
-const links = [
+type LinkItem = {
+  name: string;
+  href: string;
+  icon: React.ElementType;
+};
+
+const links: LinkItem[] = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
+  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
@@ -22,10 +23,10 @@ export default function NavLinks() {
         <a
           key={name}
           href={href}
-          className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          className="flex h-12 grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:px-3"
         >
           <Icon className="w-6" />
-          <p className="hidden md:block">{name}</p>
+          <span className="hidden md:block">{name}</span>
         </a>
       ))}
     </>
